93de4b29629278d916ed24ba92046b445c1e24a9
skilldrick <skilldrick@gmail.com>
2010-03-15 22:56:08 +0000

    This tests Qt and PIL working together.

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import sys
 from PyQt4 import QtCore, QtGui
 
+import Image
+import ImageQt
+
     
 
 class MyWidget(QtGui.QWidget):
@@ -10,6 +13,14 @@ class MyWidget(QtGui.QWidget):
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('My Widget!')
 
+        
+        im = Image.open('kitten.jpg')
+        print im.format
+        newImage = ImageQt.ImageQt(im)
+        pixmap = QtGui.QPixmap.fromImage(newImage)
+        label = QtGui.QLabel('Hello', self)
+        label.setPixmap(pixmap)
+
         quit = QtGui.QPushButton('Close', self)
         self.connect(quit, QtCore.SIGNAL('clicked()'),
                      QtGui.qApp, QtCore.SLOT('quit()'))
@@ -18,4 +29,6 @@ class MyWidget(QtGui.QWidget):
 app = QtGui.QApplication(sys.argv)
 myWidget = MyWidget()
 myWidget.show()
+
+
 sys.exit(app.exec_())
