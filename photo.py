3e44179201c4891dfff7f9fd0dd3dac7d77fcdf1
skilldrick <skilldrick@gmail.com>
2010-03-31 21:57:20 +0100

    Started to read exif data

--- photo.py
+++ photo.py
@@ -1,6 +1,7 @@
 import unittest
 import os.path
 import Image
+from PIL.ExifTags import TAGS
 
 class Photo:
     def __init__(self, root, filename):
@@ -11,8 +12,22 @@ class Photo:
         return self.image.format
 
     def isLandscape(self):
+        exif = self.getExif()
+        if exif['Orientation'] == 6:
+            return False
         width, height = self.image.size
         return width > height
+
+    def getExif(self):
+        info = self.image._getexif()
+        ret = {}
+        if info:
+            for tag, value in info.items():
+                decoded = TAGS.get(tag, tag)
+                ret[decoded] = value
+        else:
+            ret['Orientation'] = 1
+        return ret
 
 
 class PhotoTests(unittest.TestCase):
@@ -20,9 +35,11 @@ class PhotoTests(unittest.TestCase):
         self.root = 'imagesdir'
         self.filename = 'kitten.jpg'
         self.filename2 = 'kitten-portrait.jpg'
+        self.filename3 = 'DSC_0015.JPG'
         self.filetype = 'JPEG'
         self.photo = Photo(self.root, self.filename)
         self.photo2 = Photo(self.root, self.filename2)
+        self.photo3 = Photo(self.root, self.filename3)
 
     def testKittenIsJPG(self):
         self.assertEqual(self.filetype, self.photo.type())
@@ -32,6 +49,9 @@ class PhotoTests(unittest.TestCase):
 
     def testPortraitKittenIsPortrait(self):
         self.assertFalse(self.photo2.isLandscape())
+
+    def testHarmonyIsPortrait(self):
+        self.assertFalse(self.photo3.isLandscape())
 
 
 if __name__ == '__main__':
