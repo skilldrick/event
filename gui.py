a761f93a7bb71c5968dfa4e27904b9e652b5315a
skilldrick <skilldrick@gmail.com>
2010-04-02 23:28:25 +0100

    Finally worked out signals and slots in PyQt.

--- gui.py
+++ gui.py
@@ -4,19 +4,54 @@ import Image
 import ImageQt
 
 
-class MyWidget(QtGui.QWidget):
+class Stacked(QtGui.QStackedWidget):
     def __init__(self, parent=None):
+        QtGui.QStackedWidget.__init__(self, parent)
+        self.widget1 = MyWidget('kitten.jpg')
+        self.widget2 = MyWidget('imagesdir/kitten-portrait.jpg')
+        self.addWidget(self.widget1)
+        self.addWidget(self.widget2)
+
+    def nextPage(self):
+        self.setCurrentIndex(self.currentIndex() + 1)
+
+    def previousPage(self):
+        self.setCurrentIndex(self.currentIndex() - 1)
+
+        
+class MasterWidget(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.setGeometry(300, 300, 400, 400)
+        self.setWindowTitle('My Widget!')
+        layout = QtGui.QVBoxLayout()
+        self.stacked = Stacked(self)
+
+        self.previous = QtGui.QPushButton('Previous')
+        self.previous.clicked.connect(self.stacked.previousPage)
+        self.next = QtGui.QPushButton('Next')
+        self.next.clicked.connect(self.stacked.nextPage)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.previous)
+        hbox.addWidget(self.next)
+        
+        layout.addWidget(self.stacked)        
+        layout.addLayout(hbox)
+        self.setLayout(layout)
+
+
+class MyWidget(QtGui.QWidget):
+    def __init__(self, imagePath, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
         self.setGeometry(300, 300, 400, 293)
         self.setWindowTitle('My Widget!')
 
+        pixmap = self.convertImage(Image.open(imagePath))
 
-        PilImage = Image.open('kitten.jpg')
-        pixmap = self.convertImage(PilImage)
-        pixmap2 = self.convertImage(Image.open('imagesdir/DSC_0015.JPG'))
         label = QtGui.QLabel('', self)
-        label.setPixmap(pixmap2)
+        label.setPixmap(pixmap)
 
     def convertImage(self, image):
         """Takes a PIL image and returns a QtPixmap"""
@@ -29,7 +64,6 @@ class MyWidget(QtGui.QWidget):
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    myWidget = MyWidget()
-    myWidget.show()
-
+    masterWidget = MasterWidget()
+    masterWidget.show()
     sys.exit(app.exec_())
