206cb5994261cd7671ee4a248f1bb95f168ccb1d
skilldrick <skilldrick@gmail.com>
2010-04-26 22:51:05 +0100

    Started making categories.py work as a subclass of QFileSystemModel

--- gui/categoriespage.py
+++ gui/categoriespage.py
@@ -8,62 +8,20 @@ class CategoriesPage(QtGui.QWidget):
 
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        categoriesTree = self.categories.getTree()
         self.setupLayout()
 
     def setupLayout(self):
-        """
-        self.model = QtGui.QFileSystemModel(self)
-        self.model.setRootPath('~/Programming/Python/event/events')
+        self.model = Categories(self)
+        currentPath = QtCore.QDir.currentPath()
+        self.model.setRootPath(currentPath)
+        currentPathIndex = self.model.index(currentPath)
         self.view = QtGui.QTreeView()
         self.view.setModel(self.model)
+        self.view.setRootIndex(currentPathIndex)
+        for col in range(1, 4):
+            self.view.hideColumn(col)
+        self.view.setHeaderHidden(True)
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(self.view)
         self.setLayout(vbox)
-        """
 
-        self.tree = QtGui.QTreeWidget()
-        self.tree.setColumnCount(1)
-        self.tree.setHeaderLabel('Category')
-
-        topLevelText = ['Boys', 'Girls']
-        topLevelItems = self.makeListOfItems(topLevelText)
-        boys = self.makeListOfItems(['Under 8s', 'Under 9s', 'Under 10s'])
-        topLevelItems[0].insertChildren(0, boys)
-        girls = self.makeListOfItems(['Under 10s', 'Over 10s'])
-        topLevelItems[1].insertChildren(0, girls)
-        root = QtGui.QTreeWidgetItem()
-        root.setText(0, 'Rugby')
-        root.insertChildren(0, topLevelItems)
-
-        """
-        subitem = QtGui.QTreeWidgetItem()
-        subitem.setText(0, 'yo')
-        topLevelItems[0].insertChild(0, subitem)
-        """
-        """
-        root = QtGui.QTreeWidgetItem()
-        root.setText(0, 'Rugby')
-        """
-        self.tree.insertTopLevelItem(0, root)
-        
-        vbox = QtGui.QVBoxLayout()
-        vbox.addWidget(self.tree)
-        self.setLayout(vbox)
-
-    def convertTree(self):
-         pass
-
-
-        
-    def makeListOfItems(self, listOfNames):
-        listOfItems = []
-        for name in listOfNames:
-            listOfItems.append(self.makeItemFromName(name))
-        return listOfItems
-
-    def makeItemFromName(self, name):
-        item = QtGui.QTreeWidgetItem()
-        item.setText(0, name)
-        return item
-    
