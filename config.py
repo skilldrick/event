1bf0c4b82d14733fa3305c1f37d17051786838fc
skilldrick <skilldrick@gmail.com>
2010-05-09 22:04:46 +0100

    Settled on the use of minidom. Works OK, I suppose.

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
         return self._event
 
     def getSourceList(self):
-        self.removeItem()
+        #self.initConfigXml()
         return ['hello', 'hi']
 
     def removeItem(self):
@@ -32,22 +32,30 @@ class Config:
         sourcelist.removeChild(items[0])
         print dom.toprettyxml()
 
+    def addSubElement(self, dom, parent, name, contents=None):
+        newElement = dom.createElement(name)
+        parent.appendChild(newElement)
+        if not contents == None:
+            text = dom.createTextNode(contents)
+            newElement.appendChild(text)
+        return newElement
+
     def initConfigXml(self):
-        root = ET.Element('config')
+        dom = MD.parseString('<config />')
+        root = dom.childNodes[0]
+        sourcelist = self.addSubElement(dom, root, 'sourcelist')
+
         sources = [
             {'name':'Memory card', 'location':'D:/'},
             {'name':'Temp folder', 'location':'C:/Photos'}
             ]
-        sourcelist = ET.SubElement(root, 'sourcelist')
 
         for item in sources:
-            newItem = ET.SubElement(sourcelist, 'item')
-            name = ET.SubElement(newItem, 'name')
-            name.text = item['name']
-            location = ET.SubElement(newItem, 'location')
-            location.text = item['location']
+            newItem = self.addSubElement(dom, sourcelist, 'item')
+            self.addSubElement(dom, newItem, 'name', item['name'])
+            self.addSubElement(dom, newItem, 'location', item['location'])
 
-        tree = ET.ElementTree(root)
-        tree.write(self.configPath())
-        
+        file = open(self.configPath(), 'w')
+        root.writexml(file)
+        file.close()
     
