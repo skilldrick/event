f4f91a7c06b77669bbc9077c47e7117a65800ae6
skilldrick <skilldrick@gmail.com>
2010-06-16 23:40:09 +0100

    Moved importer into PhotoWidgetList

--- gui/importpage.py
+++ gui/importpage.py
@@ -5,7 +5,6 @@ from featurebroker import *
 
 class ImportPage(QtGui.QWidget):
     config = RequiredFeature('Config')
-    importer = RequiredFeature('Importer')
     widgets = []
     
     def __init__(self, parent=None):
@@ -13,23 +12,24 @@ class ImportPage(QtGui.QWidget):
         self.setupLayout()
 
     def setupLayout(self):
-        scrollArea = QtGui.QScrollArea()
         self.photoWidgetList = PhotoWidgetList()
+        scrollArea = QtGui.QScrollArea()
         scrollArea.setWidget(self.photoWidgetList)
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(scrollArea)
         self.setLayout(vbox)
 
     def setSourceDest(self, source, destination):
-        self.importer.setLocations(source, destination)
+        self.photoWidgetList.setSourceDest(source, destination)
         self.displayPictures()
 
     def displayPictures(self):
-        for pic in self.importer:
-            self.photoWidgetList.addPhoto(pic[0], pic[1])
+        self.photoWidgetList.display()
 
 
 class PhotoWidget(QtGui.QWidget):
+    thumbHeight = 100
+    
     def __init__(self, imagePath, imageRotation, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.path = imagePath
@@ -41,20 +41,32 @@ class PhotoWidget(QtGui.QWidget):
         if self.rotation:
             transform = QtGui.QTransform().rotate(90)
             pixmap = pixmap.transformed(transform)
-        pixmap = pixmap.scaledToHeight(100, QtCore.Qt.SmoothTransformation)
+        pixmap = pixmap.scaledToHeight(self.thumbHeight,
+                                       QtCore.Qt.SmoothTransformation)
         label = QtGui.QLabel('', self)
         label.setPixmap(pixmap)
-        
+        #set alignment to horizontal centre
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(label)
         self.setLayout(hbox)
 
     
 class PhotoWidgetList(QtGui.QWidget):
+    importer = RequiredFeature('Importer')
+
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.vbox = QtGui.QVBoxLayout()
         self.setLayout(self.vbox)
+
+    def setSourceDest(self, source, destination):
+        self.importer.setLocations(source, destination)
+
+    def display(self):
+        #limit number to display for testing
+        for i, pic in enumerate(self.importer):
+            if i < 2:
+                self.addPhoto(pic[0], pic[1])
 
     def addPhoto(self, imagePath, imageRotation):
         self.vbox.addWidget(PhotoWidget(imagePath, imageRotation))
